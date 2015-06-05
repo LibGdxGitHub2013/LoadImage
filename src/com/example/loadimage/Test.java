9dd41b8c13ce0781d710af7f05d41085a70f96ce
chengxiangtong <528440900@qq.com>
2015-06-05 21:56:27 +0800

    测试

---
+++ src/com/example/loadimage/Test.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.example.loadimage;
+
+import android.app.Activity;
+
+/**
+ * @author cheng
+ *
+ */
+public class Test extends Activity {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
